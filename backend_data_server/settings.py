a25508843db7876e3a0b26ab590b7fbb8d1e2526
mateoandraded <matanand@espol.edu.ec>
2026-08-17 13:49:13 -0500

    cambio 1

--- backend_data_server/settings.py
+++ backend_data_server/settings.py
@@ -27,7 +27,11 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.github.dev",
+    "https://*.app.github.dev",
     "https://dbolanos.pythonanywhere.com",
+    "http://localhost:8000",
+    "https://localhost:8000",
+    "http://127.0.0.1:8000",
 ]
 
 INSTALLED_APPS = [
